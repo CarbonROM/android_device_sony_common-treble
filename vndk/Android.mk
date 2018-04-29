90c4b877e6f50916e049c13494418a7eeca66672
Vinay Verma <vvinay@codeaurora.org>
2018-04-30 00:24:13 +0530

    yoshino-common: Disable vndk-sp creation for VNDK enabled targets.
    Change-Id: Idf17c7ddeab922c231fc3e9bb786c45dfa54684b

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+ifeq ($(BOARD_VNDK_VERSION),)
+$(warning ************* BOARD VNDK is not enabled - compiling vndk-sp ***************************)
 LOCAL_PATH := $(call my-dir)
 
 include $(LOCAL_PATH)/vndk-sp-libs.mk
@@ -54,3 +56,4 @@ LOCAL_MODULE := vndk-sp
 LOCAL_MODULE_TAGS := optional
 LOCAL_REQUIRED_MODULES := $(addsuffix .vndk-sp-gen,$(VNDK_SP_LIBRARIES))
 include $(BUILD_PHONY_PACKAGE)
+endif
