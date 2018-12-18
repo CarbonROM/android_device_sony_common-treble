195ae8389606285f7aafc033067d8bf405298003
mike dooley <mdooley@google.com>
2018-12-18 20:29:02 +0100

    common-treble: Enabling sound trigger V2_2
    V2_2 has the same functionality as V2_1 except it adds one new
    api, getModelState.
    
    Note: V2_1 will be removed in a subsequent cl.
    
    Test: built android and tested using SoundTriggerTestApp
    
    Bug: 70206501
    Change-Id: Id2647687732e79df252588d69f95d2a407e0092c

--- treble.mk
+++ treble.mk
@@ -92,8 +92,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.0-impl \
-    android.hardware.soundtrigger@2.0-service
+    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.2-service
 
 # CAMERA
 PRODUCT_PACKAGES += \
