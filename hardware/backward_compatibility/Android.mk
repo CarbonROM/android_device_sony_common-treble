55462faabbc863c5083c07c7407535a87bb840b2
Christian Oder <myself5@carbonrom.org>
2019-12-29 00:16:32 +0100

    common-treble: Cleanup and SELinux label extra root directorys
    Change-Id: I5aae02f55894d506db35acc783a1fcdf840fedba

--- hardware/backward_compatibility/Android.mk
+++ hardware/backward_compatibility/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-# Create mount points and symlinks for backward compatibility 
+# Create mount points and symlinks for backward compatibility
 FIRMWARE_MOUNT_POINT := $(TARGET_OUT_VENDOR)/firmware_mnt
 BT_FIRMWARE_MOUNT_POINT := $(TARGET_OUT_VENDOR)/bt_firmware
 DSP_MOUNT_POINT := $(TARGET_OUT_VENDOR)/dsp
@@ -18,21 +18,21 @@ $(FIRMWARE_MOUNT_POINT):
 ifneq ($(TARGET_MOUNT_POINTS_SYMLINKS),false)
 	@ln -sf /vendor/firmware_mnt $(TARGET_ROOT_OUT)/firmware
 endif
-	
+
 $(BT_FIRMWARE_MOUNT_POINT):
 	@echo "Creating $(BT_FIRMWARE_MOUNT_POINT)"
 	@mkdir -p $(TARGET_OUT_VENDOR)/bt_firmware
 ifneq ($(TARGET_MOUNT_POINTS_SYMLINKS),false)
 	@ln -sf /vendor/bt_firmware $(TARGET_ROOT_OUT)/bt_firmware
 endif
-	
+
 $(DSP_MOUNT_POINT):
 	@echo "Creating $(DSP_MOUNT_POINT)"
 	@mkdir -p $(TARGET_OUT_VENDOR)/dsp
 ifneq ($(TARGET_MOUNT_POINTS_SYMLINKS),false)
 	@ln -sf /vendor/dsp $(TARGET_ROOT_OUT)/dsp
 endif
-	
+
 $(PERSIST_MOUNT_POINT):
 	@echo "Creating $(PERSIST_MOUNT_POINT)"
 ifneq ($(TARGET_MOUNT_POINTS_SYMLINKS),false)
